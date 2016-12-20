3bb4f87c49748d01894785bc8061ec4c6bf3d8a9
Moses Mwongela <moses1889@gmail.com>
2016-12-20 23:20:11 +0000

    Top up now SimtoolKit action

--- app/src/main/java/com/sikumojaventures/betmoja/AccountActivity.java
+++ app/src/main/java/com/sikumojaventures/betmoja/AccountActivity.java
@@ -1,7 +1,14 @@
 package com.sikumojaventures.betmoja;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.sikumojaventures.betmoja.util.SystemAlertWindowService;
 
 /**
  * Created by turnkey on 12/17/2016.
@@ -11,10 +18,62 @@ public class AccountActivity extends AppCompatActivity {
 
     private String TAG = "AccountActivity";
 
+    private Button btnTopUpNow;
+
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        btnTopUpNow = (Button)findViewById(R.id.btnTopUpNow);
+        btnTopUpNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                topUpActivity();
+            }
+        });
+    }
+
+    public void topUpActivity(){
+        try{
+            Intent stk = getPackageManager().getLaunchIntentForPackage("com.android.stk");
+            if (stk != null)
+                startActivity(stk);
+            Intent intent = new Intent(this, SystemAlertWindowService.class);
+            startService(intent);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void onBackPressed() {
+        if(count == 1) {
+            if(isMyServiceRunning(SystemAlertWindowService.class)){
+                Intent intent = new Intent(this, SystemAlertWindowService.class);
+                stopService(intent);
+            }
+            count=0;
+            finish();
+        }
+        else {
+            Intent i = new Intent(AccountActivity.this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            finish();
+            count++;
+        }
+    }
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
---
+++ app/src/main/java/com/sikumojaventures/betmoja/util/SystemAlertWindowService.java
@@ -0,0 +1,76 @@
+package com.sikumojaventures.betmoja.util;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sikumojaventures.betmoja.R;
+
+/**
+ * Created by mwongela on 12/20/16.
+ */
+public class SystemAlertWindowService extends Service {
+
+    View systemAlertView;
+
+    public SystemAlertWindowService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException(getString(R.string.not_yet_implemented));
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        try {
+            String tillNumber = "Till number: 123456";
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    300, //width
+                    68, //height
+                    90,
+                    0,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                            | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                            | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT);
+            final WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            final LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            systemAlertView = inflater.inflate(R.layout.system_alert_window_layout, null);
+            ImageView img_close = (ImageView) systemAlertView.findViewById(R.id.img_close);
+            TextView tv_till_number = (TextView) systemAlertView.findViewById(R.id.tv_till_number);
+            tv_till_number.setText(tillNumber);
+            img_close.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    systemAlertView.setVisibility(View.GONE);
+                    return true;
+                }
+            });
+            params.gravity = Gravity.TOP;
+            wm.addView(systemAlertView, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (systemAlertView != null) {
+            systemAlertView.setVisibility(View.GONE);
+        }
+    }
+}
